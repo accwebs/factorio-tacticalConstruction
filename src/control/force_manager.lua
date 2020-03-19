ba8b4168e9510e4470a2dd80a446e81921c33ff6
Aaron Curley <accwebs@gmail.com>
2020-03-19 07:12:50 -0700

    experiment with switching robots following the player to the player's new force

--- src/control/force_manager.lua
+++ src/control/force_manager.lua
@@ -73,14 +73,44 @@ function force_manager.deinit_player(deinit_player)
     end
 end
 
+function force_manager.switch_player_robots_force(player, new_force)
+    if player.character ~= nil then
+        local player_logistic_points = player.character.get_logistic_point()
+        if type(player_logistic_points) == "table" then
+            for _, point in pairs(player_logistic_points) do
+                if point.valid == true then
+                    if point.logistic_network.valid == true then
+                        force_manager.switch_robots_force(point.logistic_network.robots, new_force)
+                    end
+                end
+            end
+        else
+            if player_logistic_points.valid == true then
+                if player_logistic_points.logistic_network.valid == true then
+                    force_manager.switch_robots_force(player_logistic_points.logistic_network.robots, new_force)
+                end
+            end
+        end
+    end
+end
+
+function force_manager.switch_robots_force(robots, new_force)
+    for _, robot in pairs(robots) do
+        robot.force = new_force
+    end
+end
+
 function force_manager.switch_player_to_alternative_force(player)
-    player.force = force_manager.fetch_alternative_force(player)
+    local alternative_force = force_manager.fetch_alternative_force(player)
+    force_manager.switch_player_robots_force(player, alternative_force)
+    player.force = alternative_force
 end
 
 function force_manager.restore_player_original_force(player)
     local base_name, player_id = force_manager.parse_force_name(player.force.name)
     if player_id ~= nil then
         local base_force = game.forces[base_name]
+        force_manager.switch_player_robots_force(player, base_force)
         player.force = base_force
     end
 end
