e626715c44536d664135d3c6bd1ad7f6ecc079e2
Aaron Curley <accwebs@gmail.com>
2020-03-21 15:51:10 -0700

    Switch to bounding-box subtraction to reduce number of entities we have to loop through

--- src/control/entity_manager.lua
+++ src/control/entity_manager.lua
@@ -50,63 +50,182 @@ function entity_manager.restore_entity_original_force(entity)
     entity_manager.force_manager.restore_entity_original_force(entity)
 end
 
-function entity_manager.determine_whether_to_revert_entity(entity, surface, alternative_force, skip_still_in_range)
+function entity_manager.revert_entity_if_not_in_player_range(entity, surface, alternative_force)
     local how_dirty_after = 0
-    if skip_still_in_range == true then
-        local networks_in_range = surface.find_logistic_networks_by_construction_area(entity.position, alternative_force)
-        if entity_manager.array_is_empty(networks_in_range) == true then
-            entity_manager.restore_entity_original_force(entity)
-        else
-            local player_owned_in_range = false
-            for _, network in pairs(networks_in_range) do
-                for _, player in pairs(game.players) do
-                    if player.connected == true then
-                        if player.character ~= nil then
-                            if entity_manager.force_manager.is_logistic_network_player_owned(player, network) then
-                                player_owned_in_range = true
-                                break
-                            end
+    local networks_in_range = surface.find_logistic_networks_by_construction_area(entity.position, alternative_force)
+    if entity_manager.array_is_empty(networks_in_range) == true then
+        entity_manager.restore_entity_original_force(entity)
+    else
+        local player_owned_in_range = false
+        for _, network in pairs(networks_in_range) do
+            for _, player in pairs(game.players) do
+                if player.connected == true then
+                    if player.character ~= nil then
+                        if entity_manager.force_manager.is_logistic_network_player_owned(player, network) then
+                            player_owned_in_range = true
+                            break
                         end
                     end
                 end
             end
-            if player_owned_in_range then
-                how_dirty_after = 1
-            else
-                entity_manager.restore_entity_original_force(entity)
-            end
         end
-    else
-        entity_manager.restore_entity_original_force(entity)
+        if player_owned_in_range then
+            how_dirty_after = 1
+        else
+            entity_manager.restore_entity_original_force(entity)
+        end
     end
     return how_dirty_after
 end
 
-function entity_manager.find_and_revert_out_of_range_entities(base_force, alternative_force)
-    for _, surface in pairs(game.surfaces) do
-        local entities = surface.find_entities_filtered({
-            force=alternative_force
-        })
-        for _, entity in pairs(entities) do
-            entity_manager.determine_whether_to_revert_entity(entity, surface, alternative_force, true)
-        end
+-- function entity_manager.find_and_revert_out_of_range_entities(base_force, alternative_force)
+--     for _, surface in pairs(game.surfaces) do
+--         local entities = surface.find_entities_filtered({
+--             force=alternative_force
+--         })
+--         for _, entity in pairs(entities) do
+--             entity_manager.determine_whether_to_revert_entity(entity, surface, alternative_force, true)
+--         end
+--     end
+-- end
+
+function entity_manager.subtract_rect_compute_top(rect, subtracted)
+    return {
+        left_top={
+            x=rect.left_top.x,
+            y=rect.left_top.y,
+        },
+        right_bottom={
+            x=rect.right_bottom.x,
+            y=rect.left_top.y + math.max(subtracted.left_top.y-rect.left_top.y, 0)
+        }
+    }
+end
+
+function entity_manager.subtract_rect_compute_left(rect, subtracted)
+    return {
+        left_top={
+            x=rect.left_top.x,
+            y=subtracted.left_top.y,
+        },
+        right_bottom={
+            x=rect.left_top.x + math.max(subtracted.left_top.x-rect.left_top.x, 0),
+            y=subtracted.right_bottom.y
+        }
+    }
+end
+
+function entity_manager.subtract_rect_compute_right(rect, subtracted)
+    return {
+        left_top={
+            x=subtracted.right_bottom.x,
+            y=subtracted.left_top.y,
+        },
+        right_bottom={
+            x=subtracted.right_bottom.x + math.max(rect.right_bottom.x-subtracted.right_bottom.x, 0),
+            y=subtracted.right_bottom.y
+        }
+    }
+end
+
+function entity_manager.subtract_rect_compute_bottom(rect, subtracted)
+    return {
+        left_top={
+            x=rect.left_top.x,
+            y=subtracted.right_bottom.y,
+        },
+        right_bottom={
+            x=rect.right_bottom.x,
+            y=subtracted.right_bottom.y + math.max(rect.right_bottom.y - subtracted.right_bottom.y, 0)
+        }
+    }
+end
+
+function entity_manager.subtract_rect_has_no_area(rect)
+    if ((rect.right_bottom.x - rect.left_top.x) <= 0 or (rect.right_bottom.y - rect.left_top.y) <= 0) then
+        return true
+    else
+        return false
     end
 end
 
-function entity_manager.find_and_revert_player_range_entities(base_force, alternative_force, skip_still_in_range)
+function entity_manager.subtract_bounding_box_from_boxes(source_boxes, box_to_subtract)
+    local output_boxes = {}
+    for _, source_box in pairs(source_boxes) do
+        if (box_to_subtract.right_bottom.x <= source_box.left_top.x or          -- subtr box's right is entirely to left of source left
+                box_to_subtract.right_bottom.y <= source_box.left_top.y or      -- subtr box's bottom is entirely above source top
+                box_to_subtract.left_top.x >= source_box.right_bottom.x or      -- subtr box's left is entirely to right of source right
+                box_to_subtract.left_top.y >= source_box.right_bottom.y) then   -- subtr box's top is entirely below source bottom
+            output_boxes[#output_boxes+1] = source_box  -- no overlap at all
+        else
+            local top_rect = entity_manager.subtract_rect_compute_top(source_box, box_to_subtract);
+            if entity_manager.subtract_rect_has_no_area(top_rect) == false then
+                output_boxes[#output_boxes+1] = top_rect
+            end
+            local left_rect = entity_manager.subtract_rect_compute_left(source_box, box_to_subtract);
+            if entity_manager.subtract_rect_has_no_area(left_rect) == false then
+                output_boxes[#output_boxes+1] = left_rect
+            end
+            local right_rect = entity_manager.subtract_rect_compute_right(source_box, box_to_subtract);
+            if entity_manager.subtract_rect_has_no_area(right_rect) == false then
+                output_boxes[#output_boxes+1] = right_rect
+            end
+            local bottom_rect = entity_manager.subtract_rect_compute_bottom(source_box, box_to_subtract);
+            if entity_manager.subtract_rect_has_no_area(bottom_rect) == false then
+                output_boxes[#output_boxes+1] = bottom_rect
+            end
+        end
+    end
+    return output_boxes
+end
+
+function entity_manager.subtract_current_player_ranges(alternative_force, last_bounding_box)
+    local remaining_bounding_boxes = {}
+    remaining_bounding_boxes[1] = last_bounding_box
+    for _, player in pairs(game.players) do
+        if player.connected == true then
+            if player.character ~= nil then
+                if player.force == alternative_force then
+                    if player.character.logistic_cell ~= nil then
+                        local player_construction_radius = player.character.logistic_cell.construction_radius
+                        local player_bounding_box = entity_manager.create_player_bounding_box(player.position, player_construction_radius)
+                        remaining_bounding_boxes = entity_manager.subtract_bounding_box_from_boxes(remaining_bounding_boxes, player_bounding_box)
+                    end
+                end
+            end
+        end
+    end
+    return remaining_bounding_boxes
+end
+
+function entity_manager.find_and_revert_previous_player_range_entities(base_force, alternative_force, skip_still_in_range)
     for player_index, player_state in pairs(global.tc_player_state) do
         if player_state.dirty > 0 then
             local how_dirty_after = 0
             local surface = game.surfaces[player_state.last_surface_index]
             if surface ~= nil then
-                local entities = surface.find_entities_filtered({
-                    area=player_state.last_bounding_box,
-                    force=alternative_force
-                })
-                for _, entity in pairs(entities) do
-                    local temp_dirty_result = entity_manager.determine_whether_to_revert_entity(entity, surface, alternative_force, skip_still_in_range)
-                    if temp_dirty_result > how_dirty_after then
-                        how_dirty_after = temp_dirty_result
+                if skip_still_in_range == false then
+                    local entities = surface.find_entities_filtered({
+                        area=player_state.last_bounding_box,
+                        force=alternative_force
+                    })
+                    for _, entity in pairs(entities) do
+                        entity_manager.restore_entity_original_force(entity)
+                    end
+                    how_dirty_after = 0
+                else
+                    local rectangles_to_search = entity_manager.subtract_current_player_ranges(alternative_force, player_state.last_bounding_box)
+                    for _, rectangle_to_search in pairs(rectangles_to_search) do
+                        local entities = surface.find_entities_filtered({
+                            area=rectangle_to_search,
+                            force=alternative_force
+                        })
+                        for _, entity in pairs(entities) do
+                            local temp_dirty_result = entity_manager.revert_entity_if_not_in_player_range(entity, surface, alternative_force)
+                            if temp_dirty_result > how_dirty_after then
+                                how_dirty_after = temp_dirty_result
+                            end
+                        end
                     end
                 end
             end
@@ -128,27 +247,31 @@ function entity_manager.on_built_entity(event)
     end
 end
 
+function entity_manager.create_player_bounding_box(position, construction_radius)
+    return {
+        left_top={
+            x=position.x - construction_radius,
+            y=position.y - construction_radius
+        },
+        right_bottom={
+            x=position.x + construction_radius,
+            y=position.y + construction_radius
+        }
+    }
+end
+
 function entity_manager.on_player_changed_position_player(player)
     local base_force = entity_manager.force_manager.fetch_base_force(player)
     local alternative_force = entity_manager.force_manager.fetch_alternative_force(player)
     local character = player.character
 
-    entity_manager.find_and_revert_player_range_entities(base_force, alternative_force, true)
+    entity_manager.find_and_revert_previous_player_range_entities(base_force, alternative_force, true)
 
     if player.force == alternative_force then
         if character.logistic_cell ~= nil then
             if global.tc_player_state[player.index].dirty < 2 then
                 local construction_radius = character.logistic_cell.construction_radius
-                local bounding_box = {
-                    left_top={
-                        x=player.position.x - construction_radius,
-                        y=player.position.y - construction_radius
-                    },
-                    right_bottom={
-                        x=player.position.x + construction_radius,
-                        y=player.position.y + construction_radius
-                    }
-                }
+                local bounding_box = entity_manager.create_player_bounding_box(player.position, construction_radius)
 
                 local entities = player.surface.find_entities_filtered({
                     area=bounding_box,
@@ -180,8 +303,8 @@ function entity_manager.on_player_left_game(event)
     local player = game.players[event.player_index]
     local base_force = entity_manager.force_manager.fetch_base_force(player)
     local alternative_force = entity_manager.force_manager.fetch_alternative_force(player)
-    entity_manager.find_and_revert_player_range_entities(base_force, alternative_force, false)
-    entity_manager.find_and_revert_out_of_range_entities(base_force, alternative_force)
+    entity_manager.find_and_revert_previous_player_range_entities(base_force, alternative_force, false)
+    --entity_manager.find_and_revert_out_of_range_entities(base_force, alternative_force)
 end
 
 function entity_manager.on_toggle(player, new_state)
@@ -190,8 +313,8 @@ function entity_manager.on_toggle(player, new_state)
     else
         local base_force = entity_manager.force_manager.fetch_base_force(player)
         local alternative_force = entity_manager.force_manager.fetch_alternative_force(player)
-        entity_manager.find_and_revert_player_range_entities(base_force, alternative_force, false)
-        entity_manager.find_and_revert_out_of_range_entities(base_force, alternative_force)
+        entity_manager.find_and_revert_previous_player_range_entities(base_force, alternative_force, false)
+        --entity_manager.find_and_revert_out_of_range_entities(base_force, alternative_force)
     end
 end
 
