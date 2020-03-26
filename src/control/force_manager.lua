d6647e585e616b90f67caad6b7c2a1e1feacb91a
Aaron Curley <accwebs@gmail.com>
2020-03-25 19:22:15 -0700

    Re-order methods in force_manager

--- src/control/force_manager.lua
+++ src/control/force_manager.lua
@@ -32,47 +32,6 @@ function force_manager.fetch_alternative_force(current_force)
         return current_force
     end
 end
-
-function force_manager.notify_init_player(player)
-    local base_force_name, is_force_alternative = force_manager.parse_force_name(player.force.name)
-    local alternative_force_name = force_manager._create_alternative_force_name(base_force_name)
-    if not game.forces[alternative_force_name] then
-        local base_force = game.forces[base_force_name]
-        local alternative_force = game.create_force(alternative_force_name)
-        alternative_force.set_friend(base_force, true)
-        alternative_force.set_cease_fire(base_force, true)
-        base_force.set_friend(alternative_force, true)
-        base_force.set_cease_fire(alternative_force, true)
-        alternative_force.share_chart = true
-        base_force.share_chart = true
-        force_manager._sync_all_tech_to_force(base_force, alternative_force)
-    end
-end
-
-function force_manager.notify_deinit_player(deinit_player)
-    local deinit_player_base_force_name, is_force_alternative1 = force_manager.parse_force_name(deinit_player.force.name)
-    local deinit_player_alterative_force_name = force_manager._create_alternative_force_name(deinit_player_base_force_name)
-
-    local delete_force = true
-    for _, this_player in pairs(game.players) do
-        if this_player ~= deinit_player then
-            if this_player.connected == true then
-                local this_player_base_force_name, is_force_alternative = force_manager.parse_force_name(this_player.force.name)
-                if deinit_player_base_force_name == this_player_base_force_name then
-                    delete_force = false
-                    break
-                end
-            end
-        end
-    end
-
-    if delete_force == true then
-        local base_force = game.forces[deinit_player_base_force_name]
-        local alternative_force = game.forces[deinit_player_alterative_force_name]
-        game.merge_forces(alternative_force, base_force)
-    end
-end
-
 function force_manager._is_logistic_network_player_owned(player, logistic_network)
     local good_network = 0
     for _, cell in pairs(logistic_network.cells) do
@@ -179,27 +138,6 @@ function force_manager._restore_player_logistic_request_counts(player, req_count
     end
 end
 
-function force_manager.switch_player_to_alternative_force(player)
-    local alternative_force = force_manager.fetch_alternative_force(player.force)
-    local switched_robots = force_manager._switch_player_robots_force(player, alternative_force)
-    local req_counts = force_manager._back_up_player_logistic_request_counts(player)
-    player.force = alternative_force
-    force_manager._restore_player_logistic_request_counts(player, req_counts)
-    force_manager._reattach_switched_robots_to_network(switched_robots, player)
-end
-
-function force_manager.restore_player_original_force(player)
-    local base_name, player_id = force_manager.parse_force_name(player.force.name)
-    if player_id ~= nil then
-        local base_force = game.forces[base_name]
-        local switched_robots = force_manager._switch_player_robots_force(player, base_force)
-        local req_counts = force_manager._back_up_player_logistic_request_counts(player)
-        player.force = base_force
-        force_manager._restore_player_logistic_request_counts(player, req_counts)
-        force_manager._reattach_switched_robots_to_network(switched_robots, player)
-    end
-end
-
 function force_manager._sync_all_tech_to_force(base_force, alternative_force)
     for name, tech in pairs(base_force.technologies) do
         alternative_force.technologies[name].researched = tech.researched;
@@ -246,6 +184,67 @@ function force_manager._sync_force_bonuses(base_force, alternative_force)
     alternative_force.character_health_bonus = base_force.character_health_bonus
     alternative_force.mining_drill_productivity_bonus = base_force.mining_drill_productivity_bonus
     alternative_force.train_braking_force_bonus = base_force.train_braking_force_bonus
+end
+
+function force_manager.switch_player_to_alternative_force(player)
+    local alternative_force = force_manager.fetch_alternative_force(player.force)
+    local switched_robots = force_manager._switch_player_robots_force(player, alternative_force)
+    local req_counts = force_manager._back_up_player_logistic_request_counts(player)
+    player.force = alternative_force
+    force_manager._restore_player_logistic_request_counts(player, req_counts)
+    force_manager._reattach_switched_robots_to_network(switched_robots, player)
+end
+
+function force_manager.restore_player_original_force(player)
+    local base_name, player_id = force_manager.parse_force_name(player.force.name)
+    if player_id ~= nil then
+        local base_force = game.forces[base_name]
+        local switched_robots = force_manager._switch_player_robots_force(player, base_force)
+        local req_counts = force_manager._back_up_player_logistic_request_counts(player)
+        player.force = base_force
+        force_manager._restore_player_logistic_request_counts(player, req_counts)
+        force_manager._reattach_switched_robots_to_network(switched_robots, player)
+    end
+end
+
+function force_manager.notify_init_player(player)
+    local base_force_name, is_force_alternative = force_manager.parse_force_name(player.force.name)
+    local alternative_force_name = force_manager._create_alternative_force_name(base_force_name)
+    if not game.forces[alternative_force_name] then
+        local base_force = game.forces[base_force_name]
+        local alternative_force = game.create_force(alternative_force_name)
+        alternative_force.set_friend(base_force, true)
+        alternative_force.set_cease_fire(base_force, true)
+        base_force.set_friend(alternative_force, true)
+        base_force.set_cease_fire(alternative_force, true)
+        alternative_force.share_chart = true
+        base_force.share_chart = true
+        force_manager._sync_all_tech_to_force(base_force, alternative_force)
+    end
+end
+
+function force_manager.notify_deinit_player(deinit_player)
+    local deinit_player_base_force_name, is_force_alternative1 = force_manager.parse_force_name(deinit_player.force.name)
+    local deinit_player_alterative_force_name = force_manager._create_alternative_force_name(deinit_player_base_force_name)
+
+    local delete_force = true
+    for _, this_player in pairs(game.players) do
+        if this_player ~= deinit_player then
+            if this_player.connected == true then
+                local this_player_base_force_name, is_force_alternative = force_manager.parse_force_name(this_player.force.name)
+                if deinit_player_base_force_name == this_player_base_force_name then
+                    delete_force = false
+                    break
+                end
+            end
+        end
+    end
+
+    if delete_force == true then
+        local base_force = game.forces[deinit_player_base_force_name]
+        local alternative_force = game.forces[deinit_player_alterative_force_name]
+        game.merge_forces(alternative_force, base_force)
+    end
 end
 
 function force_manager.register_events()
